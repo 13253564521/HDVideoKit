82618bef8930f10c92c1c542b04b90b1daff941b
liugaosheng <liu695880140@163.com>
2021-08-10 23:38:31 +0800

    修复版本提交；

--- HDVIdeoKit/Classes/playVideoController/HDAttentionViewController.m
+++ HDVIdeoKit/Classes/playVideoController/HDAttentionViewController.m
@@ -17,8 +17,8 @@
 #import "HDUserReportViewController.h"
 #import "HDServicesManager.h"
 #import "HDUkeInfoCenter.h"
-//#import <JFWechat/JFWechat.h>
-#import <WechatOpenSDK/WXApi.h>
+#import <JFWechat/JFWechat.h>
+//#import <WechatOpenSDK/WXApi.h>
 #import "LHDAFNetworking.h"
 #import "HDUserVideoListModel.h"
 
